f5683c4f046f6b35fc59245daf12e34a68307831
Juliette_17 <julita.oltusek@gmail.com>
2019-06-01 17:19:55 +0200

    change split of classes, add micro and macro averaged measures

--- data_loader.R
+++ data_loader.R
@@ -1,13 +1,15 @@
 # read data from csv file
 dat = read.csv("data1.csv", header = TRUE, sep = '#')
 dat_rows <- nrow(dat)
-good_ids = as.list(dat[dat$Overall > 66,2])
-bad_ids = as.list(dat[dat$Overall <= 66,2])
+# calculate 80 centil of Overall feature
+centil_80 = quantile(dat$Overall, seq(0,1, 0.20))[5]
+good_ids = as.list(dat[dat$Overall > centil_80,2])
+bad_ids = as.list(dat[dat$Overall <= centil_80,2])
 
 # create matrix with labels
 labels_mx = matrix("bad", dat_rows, 1)
-# players with Overall > 66 have label 'good' (8764), the rest have 'bad' (9443), split closest to 50:50
-labels_mx[dat$Overall > 66,1] = "good"
+# players with Overall > 66 have label 'good' (3213), the rest have 'bad' (14994), split closest to 20:80
+labels_mx[dat$Overall > centil_80,1] = "good"
 labels = as.list(labels_mx)
 
 # remove feature 'Overall'
@@ -25,8 +27,25 @@ Naive_Bayes_Model=naiveBayes(Label ~., data=dataset)
 NB_Predictions=predict(Naive_Bayes_Model, dataset)
 # confusion matrix to check accuracy
 conf_mx <- table(NB_Predictions,dataset$Label)
+
+#macro-averaging
+# accuracy for class 'bad' = 0.9786
+acc1 <- conf_mx[1,1]/(conf_mx[1,1] + conf_mx[1,2]) #bad
+# accuracy for class 'good' = 0.7066
+acc2 <- conf_mx[2,2]/(conf_mx[2,2] + conf_mx[2,1]) #good
+# macro-averaged accuracy = 0.8426 for fitting on full dataset and before feature selection
+accuracy <- (acc1 + acc2) / 2
+
+#0.91937
+prec1 <- conf_mx[1,1] / (conf_mx[1,1] + conf_mx[2,1])
+#0.9063
+prec2 <- conf_mx[2,2] / (conf_mx[2,2] + conf_mx[1,2])
+#0.9128429
+precision <- (prec1 + prec2) / 2
+
+#micro-averaging
 # number of good predictions
 ok_predictions <- conf_mx[1,1] + conf_mx[2,2]
-
-accuracy = ok_predictions/nrow(dataset)
+# micro_averaged accuracy = 0.917 for fitting on full dataset and before feature selection
+accuracy_micro <- ok_predictions/nrow(dataset)
 
