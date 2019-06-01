ec84cbbefdd4d263356c3807fb81a9ba1d7380aa
Juliette_17 <julita.oltusek@gmail.com>
2019-06-01 15:46:07 +0200

    add list of labels for good and bad players by column 'Overall'

--- data_loader.R
+++ data_loader.R
@@ -1,1 +1,8 @@
 dat = read.csv("data1.csv", header = TRUE, sep = '#')
+dat_rows <- nrow(dat)
+good_ids = as.list(dat[dat$Overall > 66,2])
+bad_ids = as.list(dat[dat$Overall <= 66,2])
+labels_mx = matrix("bad", dat_rows, 1)
+# players with Overall > 66 have label 'good' (8764), the rest have 'bad' (9443), split closest to 50:50
+labels_mx[dat$Overall > 66,1] = "good"
+labels = as.list(labels_mx)
